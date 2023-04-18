e69eed72e2fa73b255d004f5c7d75c1244a61c53
David Ordoñez <davidordonezescacena@hotmail.es>
2023-04-18 18:53:05 +0200

    show matches of each group ordered by rounds and saved teams qualified for playoffs

--- ShowInConsole/ShowInConsole.js
+++ ShowInConsole/ShowInConsole.js
@@ -7,7 +7,8 @@ export default class ShowInConsole {
   constructor(name) {
     this.name = name;
     this.groups = [];
-    this.roundGroups = [];
+    this.roundsGroups = [];
+    this.teamsForPlayOffs = [];
     this.startTournament();
   }
 
@@ -23,13 +24,13 @@ export default class ShowInConsole {
     this.organizeTeams();
     this.groups.forEach((group, index) => {
       var group = new PointsBasedLeague(`Group ${Letter(index)}`, group);
-      this.roundGroups.push(group);
+      this.roundsGroups.push(group);
     });
   }
 
   scheduleTournament() {
     this.createGroup();
-    this.roundGroups.forEach(group => {
+    this.roundsGroups.forEach(group => {
       group.scheduleMatchDays();
       const teamNames = group.teams.map(team => team.name);
       console.log('                           ');
@@ -51,14 +52,65 @@ export default class ShowInConsole {
     });
   }
 
+  showGroupTournament(group, summary) {
+    console.log(`${group.name} match day ${summary.index}`);
+    console.log(`---------------------------------`);
+    console.log('                           ');
+    summary.results.forEach(result => {
+      console.log(
+        `${result.homeTeam} ${result.homeGoals} - ${result.awayGoals} ${result.awayTeam}`,
+      );
+    });
+    console.log('                           ');
+    console.table(
+      summary.standings.map(team => {
+        return {
+          Team: team.name,
+          Points: team.points,
+          PlayedMatches: team.matchesWon + team.matchesDraw + team.matchesLost,
+          Won: team.matchesWon,
+          Drawn: team.matchesDraw,
+          Lost: team.matchesLost,
+          GoalsFor: team.goalsFor,
+          GoalsAgainst: team.goalsAgainst,
+          GoalsDiff: team.goalsFor - team.goalsAgainst,
+        };
+      }),
+    );
+  }
+
   startTournament() {
     this.scheduleTournament();
     console.log('=====================================');
     console.log('=========== Start WorldCup ==========');
     console.log('=====================================');
     console.log('                           ');
-    this.roundGroups.map(group => {
-      console.log(group);
+    this.roundsGroups.map(group => {
+      group.start();
+      group.summaries.forEach(summary => {
+        if (summary.index === 1) {
+          this.showGroupTournament(group, summary);
+        }
+      });
+    });
+    this.roundsGroups.map(group => {
+      group.summaries.forEach(summary => {
+        if (summary.index === 2) {
+          this.showGroupTournament(group, summary);
+        }
+      });
+    });
+    this.roundsGroups.map(group => {
+      group.summaries.forEach(summary => {
+        if (summary.index === 3) {
+          this.showGroupTournament(group, summary);
+          summary.standings.map((team, index) => {
+            if (index <= 1) {
+              this.teamsForPlayOffs.push(team.name);
+            }
+          });
+        }
+      });
     });
   }
 }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import ShowInConsole from './ShowInConsole/ShowInConsole.js';
 
 const show = new ShowInConsole('World Cup');
+
+console.log('equipos para play off', show.teamsForPlayOffs);
