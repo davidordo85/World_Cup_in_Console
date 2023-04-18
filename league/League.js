4e95aa785bc5dc4c9bc143dcd970e45232aed148
David Ordoñez <davidordonezescacena@hotmail.es>
2023-04-18 12:26:52 +0200

    fixes

--- league/League.js
+++ league/League.js
@@ -1,103 +1,42 @@
-export const LOCAL_TEAM = 0;
-export const AWAY_TEAM = 1;
-
 export default class League {
-  constructor(name, teams = []) {
+  constructor(name, teams = [], config = {}) {
     this.name = name;
     this.matchDaySchedule = [];
+    this.setup(config);
     this.setupTeams(teams);
+    this.summaries = [];
   }
 
-  setupTeams(teams) {
-    this.teamsCustomized = [];
-    teams.map(team => {
+  setup(config) {
+    const defaultConfig = { round: 1 };
+    this.config = Object.assign(defaultConfig, config);
+  }
+
+  setupTeams(teamNames) {
+    this.teams = [];
+    teamNames.map(team => {
       var teamCustomized = this.customizeTeams(team);
-      this.teamsCustomized.push(teamCustomized);
+      this.teams.push(teamCustomized);
     });
   }
 
-  customizeTeams(team) {
+  customizeTeams(teamName) {
     return {
-      name: team,
+      name: teamName,
       matchesWon: 0,
       matchesDraw: 0,
       matchesLost: 0,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
     };
   }
 
-  initSchedule() {
-    const numberOfMatchDays = this.teamsCustomized.length - 1;
-    const numberOfMatchesPerMatchDay = this.teamsCustomized.length / 2;
-    for (let i = 0; i < numberOfMatchDays; i++) {
-      const matchDay = [];
-      for (let j = 0; j < numberOfMatchesPerMatchDay; j++) {
-        const match = ['local team', 'away team'];
-        matchDay.push(match);
-      }
-      this.matchDaySchedule.push(matchDay);
-    }
-    console.log(this.mathDaySchedule);
-  }
-
-  getTeamNames() {
-    return this.teamsCustomized.map(team => team.name);
-  }
-
-  setLocalTeams() {
-    const teamNames = this.getTeamNames();
-    const maxHomeTeams = this.teamsCustomized.length - 2;
-    let teamIndex = 0;
-    this.matchDaySchedule.forEach(matchDay => {
-      matchDay.forEach(match => {
-        match[LOCAL_TEAM] = teamNames[teamIndex];
-        teamIndex++;
-        if (teamIndex > maxHomeTeams) {
-          teamIndex = 0;
-        }
-      });
-    });
-  }
-
-  setAwayTeams() {
-    const teamNames = this.getTeamNames();
-    const maxAwayTeams = this.teamsCustomized.length - 2;
-    let teamIndex = maxAwayTeams;
-    this.matchDaySchedule.forEach(matchDay => {
-      let firstMatchFound = false;
-      matchDay.forEach(match => {
-        if (!firstMatchFound) {
-          firstMatchFound = true;
-        } else {
-          match[AWAY_TEAM] = teamNames[teamIndex];
-          teamIndex--;
-          if (teamIndex < 0) {
-            teamIndex = maxAwayTeams;
-          }
-        }
-      });
-    });
-  }
-
-  fixLastTeamSchedule() {
-    let matchDayNumber = 1;
-    const teamNames = this.getTeamNames();
-    const lastTeamName = teamNames[teamNames.length - 1];
-    this.matchDaySchedule.forEach(matchDay => {
-      const firstMatch = matchDay[0];
-      if (matchDayNumber % 2 == 0) {
-        firstMatch[AWAY_TEAM] = firstMatch[LOCAL_TEAM];
-        firstMatch[LOCAL_TEAM] = lastTeamName;
-      } else {
-        firstMatch[AWAY_TEAM] = lastTeamName;
-      }
-      matchDayNumber++;
-    });
+  getScheduleMatchDays() {
+    this.scheduleMatchDays();
   }
 
   scheduleMatchDays() {
-    this.initSchedule();
-    this.setLocalTeams();
-    this.setAwayTeams();
-    this.fixLastTeamSchedule();
+    throw new Error('scheduleMatchDays not implemented');
   }
 }
