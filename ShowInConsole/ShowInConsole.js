b4118a46458435caf7456822c2572a5e078ec8cb
David Ordoñez <davidordonezescacena@hotmail.es>
2023-04-20 18:47:30 +0200

    world cup complete

--- ShowInConsole/ShowInConsole.js
+++ ShowInConsole/ShowInConsole.js
@@ -1,7 +1,6 @@
 import { teams } from '../teams/teams.js';
 import PointsBasedLeague from '../tournaments/playoffs/league/PointsBasedLeague.js';
 import Playoffs from '../tournaments/playoffs/playoffs/Playoffs.js';
-
 import Letter from '../utils/numberToLetter.js';
 import shuffle from '../utils/shuffle.js';
 
@@ -11,7 +10,9 @@ export default class ShowInConsole {
     this.groups = [];
     this.roundsGroups = [];
     this.teamsForPlayOffs = [];
+    this.teamsForThirdAndFourthPlace = [];
     this.startTournament();
+    //this.createPlayoffs();
   }
 
   organizeTeams() {
@@ -117,11 +118,50 @@ export default class ShowInConsole {
     this.createPlayoffs();
   }
 
+  showPlayoffs(round) {
+    console.log('                           ');
+    console.log(`============= ${round.name} Round ============`);
+    console.log('                           ');
+    round.summariesPlayoffs.forEach(summary => {
+      summary.result.forEach(result => {
+        console.log(
+          `${result.homeTeam} ${result.homeGoals} - ${result.awayGoals} ${result.awayTeam} => ${result.teamWin} `,
+        );
+      });
+    });
+    if (round.name !== 'Third and fourth place') {
+      this.teamsForPlayOffs = round.filteredTeamsWin;
+      this.teamsForThirdAndFourthPlace = round.filteredTeamsLoss;
+    }
+    if (round.name === 'Final') {
+      console.log('                           ');
+      console.log(`${this.teamsForPlayOffs} World Champion!!`);
+      console.log('                           ');
+    }
+  }
+
   createPlayoffs() {
     console.log('======================================');
     console.log('=========== Playoffs begin ===========');
     console.log('======================================');
     console.log('                           ');
     const eighths = new Playoffs('Eighths', this.teamsForPlayOffs);
+    eighths.start();
+    this.showPlayoffs(eighths);
+    const quarter = new Playoffs('Quarter', this.teamsForPlayOffs);
+    quarter.start();
+    this.showPlayoffs(quarter);
+    const semifinals = new Playoffs('Semifinals', this.teamsForPlayOffs);
+    semifinals.start();
+    this.showPlayoffs(semifinals);
+    const thirdAndFourPlace = new Playoffs(
+      'Third and fourth place',
+      this.teamsForThirdAndFourthPlace,
+    );
+    thirdAndFourPlace.start();
+    this.showPlayoffs(thirdAndFourPlace);
+    const final = new Playoffs('Final', this.teamsForPlayOffs);
+    final.start();
+    this.showPlayoffs(final);
   }
 }
